e6ab90b55eb2422d5fb532029f1ccfee817a7dbb
czz989898 <825752076@qq.com>
2019-08-31 13:58:32 +1000

    Update beyesi_get_batch.py

--- beyesi_get_batch.py
+++ beyesi_get_batch.py
@@ -30,26 +30,36 @@ def get_feature():
                 feature_word_data.write(words)
 
 
-def get_batch_train(partial = False):
+def get_batch_train(num):
     features = []
     vectorizer = CountVectorizer()
     doc = []
+    flag = False
+    start = 0
+    with open(r'E:\home work\semester3\machine learning\assignment\data\config.txt',encoding='UTF-8') as config:
+        for i in config:
+            start = int(i)
     with open(r'E:\home work\semester3\machine learning\assignment\data\feature_word_data.txt',encoding='UTF-8') as doc_source:
         count = -1
         y_data = []
+
         for i in doc_source:
             count += 1
-            if count % 500 == 0:
-                print(count)
-            if count>5000:
+            if count>=start:
+                if count % 200 == 0:
+                    print(count)
+                info = i.split('\t')
+                doc.append(info[1])
+                y_data.append(info[0])
+            if count>start+num:
+                flag = True
                 break
-            info = i.split('\t')
-            doc.append(info[1])
-            y_data.append(info[0])
     a = vectorizer.fit_transform(doc).toarray()
-    return a,y_data
+    with open(r'E:\home work\semester3\machine learning\assignment\data\config.txt','w', encoding='UTF-8') as config:
+        config.write(str(start+num))
+    return a,y_data,flag
 
 
 
 
-get_batch_train()
+#get_batch_train()
